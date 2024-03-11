3617583c459ca9181cb53d23fe91f130fdf7c297
Joël De Plaen <joel.deplaen@vu.nl>
2024-03-11 18:12:30 +0100

    test MRCNN

--- modified_classes/__init__.py
+++ modified_classes/__init__.py
@@ -1,4 +1,5 @@
 from modified_classes.modified_fast_rcnn_output_layers import ModifiedFastRCNNOutputLayers
 from modified_classes.modified_image_list import ModifiedImageList
+from modified_classes.modified_image_list import modified_MRCNN
 
-__all__ = ["ModifiedFastRCNNOutputLayers", "ModifiedImageList"]
+__all__ = ["ModifiedFastRCNNOutputLayers", "ModifiedImageList", "modified_MRCNN.py"]
